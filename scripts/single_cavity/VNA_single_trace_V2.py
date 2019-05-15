cb9fcc5d6aa370f0c014359012832296366cd996
computer 4 <umasswanglab@gmail.com>
2019-05-15 11:00:10 -0400

    some VNA measurements by yingying

--- scripts/single_cavity/VNA_single_trace_V2.py
+++ scripts/single_cavity/VNA_single_trace_V2.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.interactive(True)
 from measurement import Measurement1D
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,39 +8,125 @@ import numpy as np
 #from lib.math import fit
 import objectsharer as objsh
 import time
+import lmfit
+from matplotlib import gridspec
+import os
+import config
+import time
 #
 #plot type 0: amp and phase
 #plot type 1: real and imag
-def analysis(freqs, realdata, imagdata, fig=None):
+
+limit_for_off = 1
+
+def S21(params, x, y):
+    est = np.sqrt(params['kappa_prod'])/(-1j*(x-params['omega_c'])-(params['kappa_a'])/2.0 )
+    if np.max(np.abs(y)) < limit_for_off:
+        est = est + params['roff'] + 1j*params['ioff']
+    est = est * np.exp(1j*params['phi'])
+    
+    return np.sqrt((y.real - est.real)**2 + (y.imag - est.imag)**2)
+    
+    
+def S11(params, x, y):
+
+    est = (-1 - params['kappa_1'] / (1j*(x-params['omega_c'])-params['kappa_a']/2))*params['A']
+    return y - abs(est)
+    
+        
+def analysis(freqdata, realdata, imagdata, fit_S12, fit_S11, figname, fig=None):
+    fn = None
     if fig is None:
         fig = plt.figure()
+        gs = gridspec.GridSpec(1, 2, width_ratios=[1,1])
+        fig.add_subplot(gs[0])
+        fig.add_subplot(gs[1])
+    freqs = freqdata[0,:]
+    datas = realdata[0,:] + 1j*imagdata[0,:] 
+    datasdB = 20*np.log10(np.abs(datas))
+    if fit_S12:
+        params = lmfit.Parameters()
+        params.add('kappa_prod', value= (np.max(np.abs(datas))*0.5e6)**2.001, min = 0)#,vary = False)
+        params.add('omega_c', value=freqs[np.argmax(np.abs(datas))]*1.0001,min = freqs[np.argmax(np.abs(datas))]*0.9998, max = freqs[np.argmax(np.abs(datas))] * 1.0002)#,vary = False)
+        params.add('kappa_a', value=1e6, min = 0)#, max = 4e6)#,vary = False)
+        if np.max(np.abs(datas)) < limit_for_off:
+            params.add('roff',value = 1e-5)#,vary = False)
+            params.add('ioff',value = 1e-5)#, vary = False)
+        params.add('phi',value = -1, max = np.pi, min = -np.pi)#,vary = False)
+                
+    #    datas = realdata[0,:]+ 1j*imagdata[0,:]    
+        result = lmfit.minimize(S21, params, args=(freqs, datas))
+        lmfit.report_fit(result.params)
+        print ('total Q: ',result.params['omega_c'].value/result.params['kappa_a'].value)
+        fitdata = np.sqrt(result.params['kappa_prod'].value)/(-1j*(freqs-result.params['omega_c'].value)-(result.params['kappa_a'].value)/2.0 )
+        if np.max(np.abs(datas)) < limit_for_off:
+            fitdata = fitdata + result.params['roff'].value + 1j*result.params['ioff'].value
+        fitdata = fitdata * np.exp(1j*result.params['phi'].value)
+        fitdatadB = 20*np.log10(np.abs(fitdata))
+#    ampdata = 20*np.log10(np.sqrt(realdata[0,:]**2 + imagdata[0,:]**2))
+    if fit_S11:
+        params = lmfit.Parameters()
+        params.add('kappa_1', value=2.1305e+05)
+        params.add('omega_c', value=freqs[np.argmin(np.abs(datas))]*1.0001)
+        params.add('kappa_a', value=3.0022e+06)
+        params.add('A', value=1)
+                
+    #    datas = realdata[0,:]+ 1j*imagdata[0,:]    
+        result = lmfit.minimize(S11, params, args=(freqs, abs(datas)))
+        lmfit.report_fit(result.params)
+        print ('total Q: ',result.params['omega_c'].value/result.params['kappa_a'].value)
+        print ('coupling Q: ',result.params['omega_c'].value/result.params['kappa_1'].value)
+        fitdata = (-1 - result.params['kappa_1'].value / (-1j*(freqs-result.params['omega_c'].value)-result.params['kappa_a'].value/2))*result.params['A'].value
+        fitdatadB = 20*np.log10(np.abs(fitdata))
 
-#    if plot_type == 0:
 
-    ampdata = 20*np.log10(np.sqrt(realdata[0,:]**2 + imagdata[0,:]**2))
-    plt.plot(freqs[0,:]/float(1e9), ampdata )
+
+    fig.axes[0].plot(freqs/float(1e9), datasdB )
+#    fig.axes[0].set_title(figname)
+    plt.suptitle(figname)
+    if fit_S12 or fit_S11:
+        fig.axes[0].plot(freqs/float(1e9), fitdatadB,'--' )
     plt.xlabel('freq(GHz)')
     plt.ylabel('dB')
     
+    
 #    if plot_type == 1:
-    plt.figure()
-    plt.plot( realdata[0,:], imagdata[0,:])
+
+    fig.axes[1].plot( datas.real, datas.imag)
+    if fit_S12:
+        fig.axes[1].plot(fitdata.real,fitdata.imag, '--',label = 'total Q = %s\n freq = %sGHz'%(result.params['omega_c'].value/result.params['kappa_a'].value, result.params['omega_c'].value/1e9))
+    if fit_S11:
+        fig.axes[1].plot(fitdata.real,fitdata.imag, '--',label = 'total Q = %s\n couplingQ = %s\n freq = %sGHz'%(result.params['omega_c'].value/result.params['kappa_a'].value, result.params['omega_c'].value/result.params['kappa_1'].value,result.params['omega_c'].value/1e9))
     plt.xlabel('I')
-    plt.ylabel('Q')        
-
-
+    plt.ylabel('Q') 
+    plt.legend()       
+#    print datas.real, fitdata.real
+    if fn is None:
+        fn = os.path.join(config.datadir, 'images/%s_VNA_trace.png'%(time.strftime('%Y%m%d/%H%M%S', time.localtime())))
+    fdir = os.path.split(fn)[0]
+    if not os.path.isdir(fdir):
+        os.makedirs(fdir)
+    kwargs = dict()
+    fig.savefig(fn, **kwargs)
+    if fit_S11 or fit_S12:
+        return result.params
 class SingleTrace(Measurement1D):
 
-    def __init__(self,freqs, average_factor, avelimit, **kwargs):
+    def __init__(self,freqs, average_factor, avelimit, fit_S12, fit_S11, **kwargs):
         self.freqs =freqs
 #        self.meas_info = meas_info
 #        self.device_info = device_info
 #        plot_type = SPEC
         self.average_factor = average_factor
         self.avelimit = avelimit
+        self.fit_S12 = fit_S12
+        self.fit_S11 = fit_S11
+        self.fig = None
+        self.fit_params = None
 #        self.plot_type = plot_type
 
         super(SingleTrace, self).__init__(1, **kwargs)
+        self.figname = self.data.get_fullname()
 #        self.data.create_dataset('freqs', data = self.freqs)
         self.freqdata = self.data.create_dataset('freqs', shape=[1,len(self.freqs)])
         self.realdata = self.data.create_dataset('real', shape=[1,len(self.freqs)])
@@ -49,7 +137,7 @@ class SingleTrace(Measurement1D):
 
     def measure(self):
         # Generate and load sequences
-        print 'ok6'
+#        print 'ok6'
         VNA = self.instruments['VNA']
 
         VNA.set_start_freq(self.freqs[0])
@@ -100,7 +188,7 @@ class SingleTrace(Measurement1D):
                     
 #                    time.sleep(0.1)
                     objsh.helper.backend.main_loop(100)
-                    VNA.set_format('REAL')
+                    VNA.set_format('MLOG')
             except:
                 print 'error with async'
 #                VNA.set_interrupt(True)
@@ -132,13 +220,38 @@ class SingleTrace(Measurement1D):
         #        print self.freqdata
                 self.realdata[0,:] = reals
                 self.imagdata[0,:] = imags
-    
+                datas = reals + 1j*imags
+                self.fig = plt.figure()
+#                print 'during plotting'
+#                self.fig.add_subplot(111)
+                gs = gridspec.GridSpec(1, 2, width_ratios=[1,1])
+                self.fig.add_subplot(gs[0])
+                self.fig.add_subplot(gs[1])
+                self.fig.axes[0].plot(freqs/1e9, 20*np.log10(np.abs(datas)))
+                self.fig.axes[1].plot(reals, imags)
+                self.fig.axes[1].set_aspect('equal', 'box')
+#                plt.show()
+                self.fig.canvas.draw()
+
     
             else:
                 reals =( reals *ave + self.realdata[0,:] * count)/float(ave+count)
                 imags =( imags *ave + self.imagdata[0,:] * count)/float(ave+count)
                 self.realdata[0,:] = reals
                 self.imagdata[0,:] = imags
+                datas = reals + 1j*imags
+                self.fig.clear()
+#                print 'during plotting'
+#                self.fig.add_subplot(111)
+                gs = gridspec.GridSpec(1, 2, width_ratios=[1,1])
+                self.fig.add_subplot(gs[0])
+                self.fig.add_subplot(gs[1])
+                self.fig.axes[0].plot(freqs/1e9, 20*np.log10(np.abs(datas)))
+                self.fig.axes[1].plot(reals, imags)
+                self.fig.axes[1].set_aspect('equal', 'box')
+#                self.fig.axes[0].plot(freqs/1e9, 20*np.log10(np.abs(datas)))
+#                plt.show()
+                self.fig.canvas.draw()
 
 
             
@@ -150,4 +263,79 @@ class SingleTrace(Measurement1D):
     def analyze(self):
 #        fig = plt.figure()
         
-        analysis(self.freqdata, self.realdata, self.imagdata, fig=None)
+        self.fit_params = analysis(self.freqdata, self.realdata, self.imagdata, self.fit_S12, self.fit_S11,figname = self.figname, fig=self.fig)
+        
+        
+
+class SingleTraceNoAsync(Measurement1D):
+
+    def __init__(self, freqs, fit_S12, fit_S11, **kwargs):
+        self.freqs =freqs
+
+        self.fit_S12 = fit_S12
+        self.fit_S11 = fit_S11
+        self.fig = None
+        self.fit_params = None
+#        self.plot_type = plot_type
+
+        super(SingleTraceNoAsync, self).__init__(1, **kwargs)
+        self.figname = self.data.get_fullname()
+#        self.data.create_dataset('freqs', data = self.freqs)
+        self.freqdata = self.data.create_dataset('freqs', shape=[1,len(self.freqs)])
+        self.realdata = self.data.create_dataset('real', shape=[1,len(self.freqs)])
+        self.imagdata = self.data.create_dataset('imaginary', shape=[1,len(self.freqs)])
+
+
+
+
+    def measure(self):
+        # Generate and load sequences
+#        print 'ok6'
+        VNA = self.instruments['VNA']
+
+        prev_fmt = VNA.get_format()
+        freqs = VNA.do_get_xaxis()
+        VNA.set_format('REAL')
+        ret = VNA.do_get_yaxes()
+        reals = ret[0]
+#            reals = np.zeros(len(freqs))
+        VNA.set_format('IMAG')
+        ret = VNA.do_get_yaxes()
+        imags = ret[0]
+#            imags = np.zeros(len(freqs))
+        VNA.set_format(prev_fmt)
+        
+#        ret = VNA.do_get_data()
+        VNA.set_trigger_source('internal')
+#        amps=ret[0]
+#        phases=ret[1]
+#        print 'F = %.03f GHz --> amp = %.1f, angle = %.01f' % (freq / 1e9, np.abs(IQ), np.angle(IQ, deg=True))
+
+            
+        self.freqdata[0,:] = freqs
+#        print freqs
+#        print self.freqdata
+        self.realdata[0,:] = reals
+        self.imagdata[0,:] = imags
+        datas = reals + 1j*imags
+        self.fig = plt.figure()
+#        print 'during plotting'
+#                self.fig.add_subplot(111)
+        gs = gridspec.GridSpec(1, 2, width_ratios=[1,1])
+        self.fig.add_subplot(gs[0])
+        self.fig.add_subplot(gs[1])
+        self.fig.axes[0].plot(freqs/1e9, 20*np.log10(np.abs(datas)))
+        self.fig.axes[1].plot(reals, imags)
+        self.fig.axes[1].set_aspect('equal', 'box')
+#                plt.show()
+        self.fig.canvas.draw()
+
+    
+
+        self.analyze()
+
+        
+    def analyze(self):
+#        fig = plt.figure()
+        
+        self.fit_params = analysis(self.freqdata, self.realdata, self.imagdata, self.fit_S12, self.fit_S11, figname = self.figname, fig=self.fig)
--- test - VNA.py
+++ test - VNA.py
@@ -3,12 +3,14 @@ matplotlib.interactive(True)
 import mclient
 reload(mclient)
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 from pulseseq import sequencer, pulselib
 import matplotlib as mpl
 #from t1t2_plotting import smart_T1_delays
 import math as math
-
+import datetime
+import time
+import os
 #mpl.rcParams['figure.figsize']=[5,3.5]
 #mpl.rcParams['axes.color_cycle'] = ['b', 'g', 'r', 'c', 'm', 'k']
 VNA = mclient.instruments['VNA']
@@ -42,22 +44,82 @@ if 0: #sweep voltage Yoko and get 2D plot
     VNA.set_trigger_source('internal')
     ro = VNA_sweep_yoko.Sweep_YOKO(volts = np.linspace(4.5,5.7,101), freqs = np.linspace(8e9, 9e9, 1601), average_factor =5, fig_name ='YIG in Drilled Cavity, Take 1')
     ro.measure()
-    plt.show()
+    pl.show()
     bla
     
 if 0: #sweep current Yoko and get 2D plot
     from scripts.single_cavity import VNA_current_sweep_yoko 
-    Yoko.do_set_output_state(1)
+    Yoko.do_set_output_state(0)
     VNA.set_timeout(40000)
     VNA.do_enable_averaging(True)
     VNA.set_averaging_trigger(1)
     VNA.set_trigger_source('internal')
-    ro = VNA_current_sweep_yoko.Current_Sweep_YOKO(currents = np.linspace(0.5,0.6,201), freqs = np.linspace(8.0e9, 9.0e9, 201),
-                                                   average_factor =1, Sij =['S12'], fig_name ='YIG in Cavity')
+    ro = VNA_current_sweep_yoko.Current_Sweep_YOKO(currents = np.linspace(0,0.1,11), freqs = np.linspace(8.0e9, 9.0e9, 1601),
+                                                   average_factor =1, avelimit = 1,if_bandwidth = 1000, Sij =['S21'],fig_name ='test',comment = 'with Yoko, and VNA data')
     #we can take all 4 S params data at the same time when VNA is calibrated, if not, we can only take the data with same output ports at the same time. 
     ro.measure()
-    plt.show()
+    pl.show()
     bla
+
+
+if 1: #sweep power and get 2D plot
+    from scripts.single_cavity import Magnet_sweep_VNA
+    VNA.set_timeout(40000)
+    VNA.do_enable_averaging(True)
+    VNA.set_averaging_trigger(1)
+    VNA.set_trigger_source('internal')
+    
+    a = [0,0,0,0]
+    a[0] = np.linspace(0,0.1,101)
+    a[1] = np.linspace(0.1,0,101)
+    a[2] = np.linspace(0,-0.1,101)
+    a[3] = np.linspace(-0.1,0,101)
+    for i in range(4):
+        
+    #    a= np.log10(a)*10
+    #    a[0] = -11
+        ro = Magnet_sweep_VNA.Magnet_Sweep_VNA(fields = a[i], freqs = np.linspace(5.5665e9, 5.5725e9, 101),
+                                                       average_factor =10, avelimit =3,if_bandwidth = 10, Sij =['S21'],fig_name ='field sweep',comment = 'qubit in waveguide')
+        #we can take all 4 S params data at the same time when VNA is calibrated, if not, we can only take the data with same output ports at the same time. 
+        ro.measure()
+        pl.show()
+    bla
+    
+if 0: #sweep power and get 2D plot
+    from scripts.single_cavity import power_sweep_VNA
+    VNA.set_timeout(40000)
+    VNA.do_enable_averaging(True)
+    VNA.set_averaging_trigger(1)
+    VNA.set_trigger_source('internal')
+    a = np.linspace(0,1,101)
+#    a= np.log10(a)*10
+#    a[0] = -11
+    ro = power_sweep_VNA.Power_Sweep_VNA(powers = a, freqs = np.linspace(5.5665e9, 5.5725e9, 101),
+                                                   average_factor =10, avelimit =3,if_bandwidth = 10, Sij =['S21'],fig_name ='no pump',comment = 'qubit in waveguide')
+    #we can take all 4 S params data at the same time when VNA is calibrated, if not, we can only take the data with same output ports at the same time. 
+    ro.measure()
+    pl.show()
+    bla
+
+if 0: #sweep power and changing frequency
+    from scripts.single_cavity import power_sweep_varies_freq_VNA
+    VNA.set_timeout(40000)
+    VNA.do_enable_averaging(True)
+    VNA.set_averaging_trigger(1)
+    VNA.set_trigger_source('internal')
+    a = np.linspace(0,1,21)
+    center_freq = -0.1739* a + 5.5604
+    center_freq = center_freq * 1e9
+    a= np.log10(a)*10
+    a[0] = -14
+    ro = power_sweep_varies_freq_VNA.Power_Sweep_Varies_freq_VNA(powers = a, center_freqs = center_freq, span = 30e6, VNA_points = 401,
+                                                   average_factor =500, avelimit =10,if_bandwidth = 100, Sij =['S21'],fig_name ='pump power sweep ',comment = 'power sweep for qubit in waveguide')
+    #we can take all 4 S params data at the same time when VNA is calibrated, if not, we can only take the data with same output ports at the same time. 
+    ro.measure()
+    pl.show()
+    bla
+
+
 
 if 0: #get single trace from VNA, for long meaasurements
     from scripts.single_cavity import VNA_single_trace_V2
@@ -66,14 +128,117 @@ if 0: #get single trace from VNA, for long meaasurements
     VNA.do_enable_averaging(True)
     VNA.set_averaging_trigger(1)
     VNA.set_trigger_source('internal')
-    ro = VNA_single_trace_V2.SingleTrace(freqs = np.linspace(8e9, 9e9, 1601), average_factor = 980,avelimit = 1)
+    ro = VNA_single_trace_V2.SingleTrace(freqs = np.linspace(5.5639e9, 5.5739e9, 401), average_factor = 40, avelimit = 2, fit_S12 = 0, fit_S11 =0)
 
-    print 'ok3'
+#    print 'ok3'
     ro.measure()
 #    print 'ok4'
 #    a=ro.ampdata
 #    b= ro.freqdata
 #    print 'ok5'
-    plt.show()
-    #bla 
+    pl.show()
+    bla 
 
+if 0: #get single trace from VNA, withoout waiting, just take screenshot and fit it.
+    from scripts.single_cavity import VNA_single_trace_V2
+#    print 'OK2'
+    freqs = VNA.do_get_xaxis()
+    ro = VNA_single_trace_V2.SingleTraceNoAsync(freqs, fit_S12 = 1, fit_S11 =0)
+
+    ro.measure()
+
+    pl.show()
+    bla
+    
+    
+    
+if 0: #sweep field to 
+    from scripts.single_cavity import VNA_single_trace_V2
+    VNA.set_timeout(40000)
+    VNA.do_enable_averaging(True)
+    VNA.set_averaging_trigger(1)
+    VNA.set_trigger_source('internal')   
+    average = 1000
+    '''Create all your empty arrays to save fit parameters in'''    
+    m_list = np.linspace(0,20,11)
+    
+    time_of_m = np.zeros(len(m_list))
+    kappa_result = np.zeros(len(m_list))
+    kappa_err = np.zeros(len(m_list))
+    Q_result = np.zeros(len(m_list))
+
+    '''Just helps with bookkeeping later on during data analysis'''
+    start_time = list(str(datetime.datetime.now())[:19])
+    start_time[13] = '-'
+    start_time[16] = '-'
+#    magnet = mclient.instruments['Magnet']
+    SCqubit = mclient.instruments['SCqubit']
+    for i,m in enumerate(m_list):
+#        magnet.do_set_field(m)
+        SCqubit.set_power(m)
+#        time.sleep(5)       
+        
+    #    print 'OK2'
+        time_of_m[i] = int(time.strftime('%H%M%S'))
+
+        print '###############'
+        print m,'dB'
+        print '##############'
+        ro = VNA_single_trace_V2.SingleTrace(freqs = np.linspace(5.42e9, 5.72e9, 401), average_factor = average, avelimit = 10, fit_S12 = 1, fit_S11 =0)
+
+        ro.measure()
+        kappa_result[i] = ro.fit_params['kappa_a'].value
+#        pl.savefig(save_filepath + 'kappas.png')
+        pl.close()
+        
+
+
+    '''More bookkeeping'''
+    
+    end_time = list(str(datetime.datetime.now())[:19])
+    end_time[13] = '-'
+    end_time[16] = '-'
+    
+#    '''These are just showing you how good your fits were for this run'''
+#    print('Average percent error on %.0f T1 measurements was %.03f:' %(int(N), np.average(t1_err/t1_result)))
+#    print('Average percent error on %.0f T2 measurements was %.03f:' %(int(N), np.average(t2_err/t2_result)))
+    
+    '''Save the data for later analysis'''
+    main_filepath = 'C:/Users/Wang_Lab/Documents/yingying/0418cooldown/field sweep//no_field'
+    time_stamp = start_time + list(str(' to ')) + end_time
+    save_filepath = main_filepath + ''.join(time_stamp) + '/'
+    
+    
+    if not os.path.exists(save_filepath):
+        os.makedirs(save_filepath)
+        
+    np.savetxt(save_filepath + 'results.txt',
+               np.column_stack((m_list,time_of_m, kappa_result, kappa_err, Q_result)),
+               header = 
+               
+               'field,time_of_field, kappa_result, kappa_err, Q_result')
+    
+    np.savetxt(save_filepath + 'notes.txt', [0],
+               header = 
+               'm_list = np.linspace(' + str(m_list[0]) +',' + str(m_list[-1]) + ',' + str(len(m_list)) + ')'  +
+               ', num_averages = ' + str(average) )
+    
+    '''Plot the data'''
+    pl.figure()
+    pl.errorbar(m_list, kappa_result/1000000, yerr = kappa_err/1000000, fmt ='o', label='kappa_tot')
+#    pl.xlabel('field(T)')
+#    pl.xlabel('different measurement')
+    pl.xlabel('drive power')
+    pl.ylabel('linewidth(MHz)')
+    pl.legend(loc='upper right')
+    pl.savefig(save_filepath + 'kappas.png')
+    pl.figure()
+    pl.scatter(m_list, Q_result, label='total Q')
+#    pl.xlabel('field(T)')
+#    pl.xlabel('different measurement')
+    pl.xlabel('drive power')
+    pl.ylabel('Q')
+    pl.legend(loc='upper right')
+    pl.savefig(save_filepath + 'Qs.png')
+    
+#    magnet.do_set_field(0)
