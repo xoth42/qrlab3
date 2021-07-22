ae0babab58132e4f958a52a79dd24b6297f9b9a7
PC9 <umasswanglab@gmail.com>
2021-07-21 21:02:34 -0400

    new file:   OPENTASKS 	new file:   QRLab.bat 	new file:   clear.bat 	new file:   start_QRLab.bat

---
+++ QRLab.bat
@@ -0,0 +1,3 @@
+@echo off
+title QRLab Server
+python tk_gui.py
---
+++ clear.bat
@@ -0,0 +1,7 @@
+@echo off
+
+TASKKILL /FI "WINDOWTITLE eq Instrument Server*" /T /F >nul 2>&1
+TASKKILL /FI "WINDOWTITLE eq Data Server*"  /T  /F >nul 2>&1
+TASKKILL /FI "WINDOWTITLE eq QRLab Server*"  /T  /F >nul 2>&1
+TASKKILL /FI "WINDOWTITLE eq QRLab*"  /T  /F >nul 2>&1
+exit
